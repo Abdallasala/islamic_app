d164d1ca6a7af63cd32db791df06ad9f70dd041a
AbdallaSala <abdallasalah329@gmail.com>
2024-12-29 12:35:28 +0200

    finish splash and onboarding

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,9 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:islamic_app/main.dart';
 
+//import 'package:islami_c13_sun/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
